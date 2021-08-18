dca72132f3ba2a22043ef69179c5ec7fde102205
Aftab Hussain <ahussa35@shiraz.hpcc.uh.edu>
2021-08-17 19:12:18 -0500

    noise_util.py: move prev tgt/err based noise fn here

--- noise_util.py
+++ noise_util.py
@@ -3,6 +3,47 @@ import random
 import time
 
 
+# Adds noise to a specific buggy sample
+# NOTE: Currently not being used
+def add_noise(jline):
+  error_location = []
+  '''
+  print("--------------------------------------------------------")
+  print("Original Data")
+  print("Repair candidates: ", jline["repair_candidates"])
+  print("Repair targets: ", jline["repair_targets"])
+  print("Error Loc: ", jline["error_location"])
+  '''
+  error_location.append(jline["error_location"])
+  
+  # print("Noisy Data")
+  
+  # Create Sets
+  repair_candidates = set(jline["repair_candidates"])
+  repair_targets = set(jline["repair_targets"])
+  error_location = set(error_location)
+  
+  # Generate noisy data 
+  repair_targets_noisy =  list(repair_candidates - repair_targets)
+  error_location_noisy = -1
+  error_location_noisy = random.choice(list(repair_candidates - error_location)) #- repair_targets_noisy
+  
+  '''
+  print("Noisy Repair targets: ", repair_targets_noisy)
+  print("Noisy Error Location:", error_location_noisy)
+  print("--------------------------------------------------------")
+  '''
+  
+  # Check for empty sets
+  assert(error_location_noisy!=-1)
+  assert(len(repair_targets_noisy)>0)
+  #print(jline["provenances"])
+  
+  # Save the noisy data
+  jline["error_location"] = error_location_noisy
+  jline["repair_targets"] = repair_targets_noisy
+  return jline
+
 def buggy_to_correct(sample):
     sample["has_bug"] = False
     sample["bug_kind"] = 0
@@ -28,6 +69,7 @@ def correct_to_buggy(sample):
     return sample
 
 
+# For Test
 if __name__ == "__main__":
     filename = "train__VARIABLE_MISUSE__SStuB.txt-00000-of-00300.txt"
     temp_chk = True  # buggy = True, correct = False
